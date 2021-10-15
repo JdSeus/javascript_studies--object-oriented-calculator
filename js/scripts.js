70f74733c4c02911f2a0fbaee7d4d63acb1ccd1b
Juliano Seus <julianoseusdev@outlook.com>
2021-10-15 17:07:16 -0300

    Comecando Logica dos Botoes

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,36 @@ class Calculator {
         this.reset = 0;
     }
 
+    checkLastDigit(input, upperValue, reg) {
+        if((
+            !reg.test(input) &&
+            !reg.test(upperValue.substr(upperValue.length - 1))
+        )) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     btnPress() {
-        console.log(this)
+
+        let input = this.textContent;
+        let upperValue = calc.upperValue.textContent;
+
+        //Verificar se há apenas números
+        var reg = new RegExp('^\\d+$');
+
+        //Checa se precisa adicionar Sinal ou não
+        if (calc.checkLastDigit(input, upperValue, reg)) {
+            return false;
+        }
+
+        if (upperValue == "0") {
+            calc.upperValue.textContent = input;
+        } else {
+            calc.upperValue.textContent += input;
+        }
+
     }
 
 }
